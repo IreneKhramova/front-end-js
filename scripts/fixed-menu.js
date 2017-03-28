48d239a3a865f58130c10c4a461e7cfdd9d1a104
Irene Khramova <Irinka379@yandex.ru>
2017-03-28 13:48:57 +0300

    Fixes goodsPaddingTop

--- scripts/fixed-menu.js
+++ scripts/fixed-menu.js
@@ -2,16 +2,17 @@ var menu = document.querySelector('.menu');
 var menuHeight = menu.offsetHeight;
 var goods = document.querySelector(".goods");
 var menuPos = menu.getBoundingClientRect();
+var goodsPaddingTop = parseInt(getComputedStyle(goods).paddingTop);
 
 document.addEventListener('scroll', function (e) {
 	var scrolled = window.pageYOffset;
-
+    var goodsStyle = getComputedStyle(goods);
 	if(scrolled > menuPos.top) {
         menu.classList.add('scrolled');
-        goods.style.paddingTop = (menuHeight+30) + "px";
+        goods.style.paddingTop = (menuHeight+goodsPaddingTop) + "px";
     }   
     else {     
         menu.classList.remove('scrolled');
-        goods.style.paddingTop = "30px";
+        goods.style.paddingTop = "";
     }
 });
